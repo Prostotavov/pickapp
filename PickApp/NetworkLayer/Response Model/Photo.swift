c4808ddd9689a83741be05bc148973e6a5506920
Prostotavov <romasenkevich@bk.ru>
2024-02-04 14:27:50 +0300

    [FIXED] photo model

--- PickApp/NetworkLayer/Response Model/Photo.swift
+++ PickApp/NetworkLayer/Response Model/Photo.swift
@@ -10,9 +10,16 @@ import Foundation
 // MARK: - Photo
 struct Photo: Codable {
     let id, slug, description, altDescription: String?
+    let urls: PhotoURL?
 
     enum CodingKeys: String, CodingKey {
-        case id, slug, description
+        case id, slug, description, urls
         case altDescription = "alt_description"
     }
+}
+
+// MARK: - PhotoURL
+struct PhotoURL: Codable {
+    let raw, full, regular, small: String
+    let thumb: String
 }
